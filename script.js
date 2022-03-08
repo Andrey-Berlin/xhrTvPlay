4f9cfbac870140857aa210dfb4affce86b91c6c7
Андрей Берлин <99011598+Andrey-Berlin@users.noreply.github.com>
2022-03-08 13:31:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+
+  
+  
+  let style = document.getElementById('style');
+  style.innerHTML = `
+  audio{
+    margin: 30px 0px 0px 0px;
+    
+  }
+  
+  ul {
+    padding: 0px 30px 0px 50px;
+  }
+  
+  video {
+    
+    width: 100%;
+    margin: 30px 0px 0px 0px;
+  }
+p{
+    color:rgb(255,255,255);
+    background:#000000;
+    
+    
+  }
+section {
+  text-align:center;
+}  
+  `;
+let header = document.getElementById('header');
+header.innerHTML = `
+<h1>TvApp</h1>
+<p>Внимание! Это приложение еще в стадии разроботки.</p>
+
+`;
+function liveVideo(tv) {
+ let section = document.getElementById("section").innerHTML = tv;
+
+
+}
+
+function getFile(netWeb) {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', "video-player.txt");
+  xhr.onload = function() {
+    if (xhr.status == 200) {
+      netWeb(this.responseText);
+    } else {
+      netWeb("Error: " + xhr.status);
+    }
+  }
+  xhr.send();
+}
+
+
+getFile(liveVideo); 
+//let section = document.getElementById('section');
+//section.innerHTML = ``;
+
+/*section.style.margin = '1em';*/
+/*let video1 = document.getElementById("video1");
+video1.innerHTML = ``;
+let video2 = document.getElementById('video2');
+video2.innerHTML = ``;*/
+/*audio.style.fontSize = '50px';*/
+
+
+
